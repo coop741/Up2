59246125e14de84266fcc557e83c688d8be531b0
Avonlea Haymart <avhaymart@gmail.com>
2019-02-20 14:05:44 -0700

    Component Structure

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Post.css'
+
+class Post extends Component {
+    render() {
+        return (
+            <div className="post">
+            </div>
+        )
+    }
+}
+
+export default Post;
---
+++ src/components/PostCreate/PostCreate.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './PostCreate.css'
+
+class PostCreate extends Component {
+    render() {
+        return (
+            <div className="post-create">
+                
+            </div>
+        )
+    }
+}
+
+export default PostCreate;
