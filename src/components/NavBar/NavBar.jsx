d44375927a2d17f893aac29868fae0cf05334a7f
Avonlea Haymart <avhaymart@gmail.com>
2019-02-20 14:49:13 -0700

    Added navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,11 +4,16 @@ import './NavBar.css'
 class NavBar extends Component {
     render() {
         return (
-            <div className="navbar">
-                
-            </div>
-        )
-    }
-}
+            <nav class="navbar navbar-dark bg-dark justify-content-between">
+                <img src="./images/logo.png" alt="Logo"></img>
 
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </nav>
+                )
+            }
+        }
+        
 export default NavBar;
