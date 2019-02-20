4f485644192a550aa2c249b5962fe81f084c4c22
Avonlea Haymart <avhaymart@gmail.com>
2019-02-20 14:51:12 -0700

    Added navbar container

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,15 +5,17 @@ class NavBar extends Component {
     render() {
         return (
             <nav class="navbar navbar-dark bg-dark justify-content-between">
-                <img src="./images/logo.png" alt="Logo"></img>
+                <div className="container">
+                    <img src="./images/logo.png" alt="Logo"></img>
 
-                <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                    <form class="form-inline">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
+                    </form>
+                </div>
             </nav>
-                )
-            }
-        }
-        
+        )
+    }
+}
+
 export default NavBar;
