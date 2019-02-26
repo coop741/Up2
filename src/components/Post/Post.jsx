8c4b065cdd557f179226b4e71c3d917eaf159c94
Avonlea Haymart <avhaymart@gmail.com>
2019-02-26 09:30:05 -0700

    MySQL DB Comments workaround

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import {Modal, Button} from 'react-bootstrap'
+import { Modal, Button } from 'react-bootstrap'
 import Comment from "../Comment";
 import "./Post.css";
 
 class Post extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
       comments: [],
@@ -25,7 +25,10 @@ class Post extends Component {
 
   componentDidMount() {
     console.log(this.props.description)
-    this.setState({comments: this.props.comments})
+    if (this.props.comments !== undefined) {
+      console.log("Comments exist")
+      this.setState({ comments: this.props.comments })
+    } else {console.log("comments don't exist in db")}
   }
 
   render() {
@@ -33,26 +36,26 @@ class Post extends Component {
     return (
       // {/*  date, value, hashtags */}
       <>
-      <div className="col-md-4">
-        <div className="post p-3">
-          <div className="card">
-            <img className="card-img-top" src={image} alt="post" />
-            <div className="card-body">
-              <h5 className="card-title">
-                {title}
-                <span className="badge badge-secondary ml-2">{value}</span>
-                <br/>
-                <Button variant="primary" size="sm" onClick={this.handleShow}>Read More</Button>
-              </h5>
-              <p className="card-text">{description}</p>
-            </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item text-muted">
-                Written by {first} {last}
-              </li>
-              <li className="list-group-item text-muted">Posted {date}</li>
+        <div className="col-md-4">
+          <div className="post p-3">
+            <div className="card">
+              <img className="card-img-top" src={image} alt="post" />
+              <div className="card-body">
+                <h5 className="card-title">
+                  {title}
+                  <span className="badge badge-secondary ml-2">{value}</span>
+                  <br />
+                  <Button variant="primary" size="sm" onClick={this.handleShow}>Read More</Button>
+                </h5>
+                <p className="card-text">{description}</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item text-muted">
+                  Written by {first} {last}
+                </li>
+                <li className="list-group-item text-muted">Posted {date}</li>
 
-              {/* <li className="list-group-item">
+                {/* <li className="list-group-item">
                 <p>
                   {hashtags.map(item => (
                     <span key={item} className="badge badge-secondary">
@@ -61,39 +64,42 @@ class Post extends Component {
                   ))}
                 </p>
               </li> */}
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
 
-      <Modal show={this.state.show} onHide={this.handleClose} size="lg">
-      <Modal.Header closeButton>
-        <Modal.Title>
-          {title}
-          <img className="card-img-top" src={image} alt="post" />
-        </Modal.Title>
-      </Modal.Header>
+        <Modal show={this.state.show} onHide={this.handleClose} size="lg">
+          <Modal.Header closeButton>
+            <Modal.Title>
+              {title}
+              <img className="card-img-top" src={image} alt="post" />
+            </Modal.Title>
+          </Modal.Header>
 
-      <Modal.Body>
-        <p>{description}</p>
-        <div>
-          {this.state.comments.length ? (
-            this.state.comments.map(singleComment => <Comment id={singleComment} />)
-          ) : (
-            <p>- - - - </p>
-          )}
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <p>Written by: {first} {last} on {date}</p>
-        <Button variant="secondary" onClick={this.handleClose}>
-          Close
+          <Modal.Body>
+            <p>{description}</p>
+            <div>
+              {
+                undefined !== this.state.comments && this.state.comments.length !== 0 &&
+                  this.state.comments.length ? (
+                    this.state.comments.map(singleComment => <Comment id={singleComment} />)
+                  ) : (
+                    <p>- - - - </p>
+                  )
+              }
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <p>Written by: {first} {last} on {date}</p>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
         </Button>
-        <Button variant="primary" onClick={this.handleClose}>
-          Post Comment
+            <Button variant="primary" onClick={this.handleClose}>
+              Post Comment
         </Button>
-      </Modal.Footer>
-      </Modal>
+          </Modal.Footer>
+        </Modal>
       </>
     );
   }
